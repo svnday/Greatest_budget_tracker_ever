9ebb19ac21d06bb6938cb3fb383ae29893ac1bd3
svnday <40030806+svnday@users.noreply.github.com>
2022-03-28 18:54:25 -0500

    typo in manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,7 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/css/styles.css",
     "/js/index.js",
-    "/js/db.js",
+    "/js/idb.js",
     "/icons/icon-72x72.png",
     "/icons/icon-96x96.png",
     "/icons/icon-128x128.png",
